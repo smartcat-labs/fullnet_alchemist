6ca90e99b61655412cbd16424c73b3fa496c31ab
stanko <stankokuveljic@gmail.com>
2018-05-15 12:40:57 +0200

    Add implementation of resnet

--- fullnet_alchemist/models/generative/super_resolution_resnet.py
+++ fullnet_alchemist/models/generative/super_resolution_resnet.py
@@ -5,16 +5,40 @@ import tensorflow.contrib.slim as slim
 
 from fullnet_alchemist.utils.summaries import variable_summaries
 
-# TODO: This implementation is invalid
 
-class SimpleGenerator(object):
+def resnet_block(input_tensor, num_outputs, name,  layers_num=2):
+    output_tensor = input_tensor
+    with tf.variable_scope(name):
+        for conv_i in range(layers_num):
+            output_tensor = slim.conv2d(output_tensor, num_outputs=num_outputs, scope="{}_{}_conv2d".format(name, conv_i))
 
-    def __init__(self, conv_layers, upsampling_layers, kernel, activation_fn=tf.nn.relu):
-        self.conv_layers = conv_layers
-        self.upsampling_layers = upsampling_layers
-        self.kernel = kernel
-        self.activation_fn = activation_fn
+    return output_tensor
+
+
+def parametric_relu_fn():
+    def prelu(input_tensor):
+        with tf.variable_scope("prelu"):
+            alpha = tf.get_variable("alpha",
+                                    shape=input_tensor.get_shape()[-1],
+                                    dtype=input_tensor.dtype,
+                                    initializer=tf.constant_initializer(0.1))
+            return tf.maximum(0.0, input_tensor) + alpha * tf.minimum(0.0, input_tensor)
+
+    return prelu
+
+
+class Generator(object):
+
+    def __init__(self, activation_fn, resblocks_num=3, resnet_output_num=64, upsampling_output_num=256,
+                 external_kernel_size=9, internal_kernel_size=3, upsampling_layers=2):
         self.reuse = False
+        self.activation_fn = activation_fn
+        self.resnet_output_num = resnet_output_num
+        self.external_kernel_size = external_kernel_size
+        self.internal_kernel_size = internal_kernel_size
+        self.resblocks_num = resblocks_num
+        self.upsampling_layers_num = upsampling_layers
+        self.upsampling_output_num = upsampling_output_num
 
         self.normalizer_fn = slim.batch_norm
         self.normalizer_params = {
@@ -27,47 +51,85 @@ class SimpleGenerator(object):
 
         }
 
+    def _resblocks(self, input_tensor, is_training=True):
+        block_input = input_tensor
+        norm_params = dict(self.normalizer_params.items() + [('is_training', is_training)])
+
+        with tf.variable_scope("res_blocks"),\
+                     slim.arg_scope([slim.conv2d],
+                                    kernel_size=self.internal_kernel_size,
+                                    activation_fn=self.activation_fn,
+                                    normalizer_fn=self.normalizer_fn,
+                                    normalizer_params=norm_params,
+                                    stride=1,
+                                    padding='SAME'):
+            for res_block_i in range(self.resblocks_num):
+                with tf.variable_scope("resblock_{}".format(res_block_i)):
+                    block_output = block_input + resnet_block(block_input, self.resnet_output_num, str(res_block_i), layers_num=2)
+                    block_input = block_output
+
+            with tf.variable_scope("last_conv"):
+                block_output = slim.conv2d(block_input,
+                                           num_outputs=self.resnet_output_num,
+                                           activation_fn=None,
+                                           scope="conv2d_{}x{}".format(self.internal_kernel_size, self.internal_kernel_size))
+
+        return block_output
+
+    def _upsampling_layers(self, input_tensor):
+        outputs = input_tensor
+        with tf.variable_scope("upsampling_blocks"), slim.arg_scope([slim.conv2d],
+                                                                    kernel_size=3,
+                                                                    num_outputs=self.upsampling_output_num,
+                                                                    activation_fn=None,
+                                                                    normalizer_fn=None,
+                                                                    stride=1
+                                                                    ):
+            for upsampling_i in range(self.upsampling_layers_num):
+                with tf.variable_scope("upsampling_{}".format(upsampling_i)):
+                    outputs = slim.conv2d(outputs,
+                                          scope="conv2d_{}x{}".format(self.internal_kernel_size, self.internal_kernel_size))
+                    outputs = tf.depth_to_space(outputs, 2, name="subpixel_{}".format(upsampling_i))
+                    outputs = self.activation_fn(outputs)
+
+        return outputs
+
     def generate(self, input_tensor, name, is_training=True):
 
-        norm_params = dict(self.normalizer_params.items() + [('is_training', is_training)])
-        outputs = input_tensor
-        with tf.name_scope(name=name),\
-             tf.variable_scope("generator", reuse=self.reuse):
+        output_tensor = input_tensor
 
-            with slim.arg_scope([slim.conv2d, slim.conv2d_transpose],
-                                kernel_size=self.kernel,
-                                stride=1,
-                                activation_fn=self.activation_fn,
-                                normalizer_fn=self.normalizer_fn,
-                                normalizer_params=norm_params,
-                                padding='SAME'
-                                ):
-                for conv_i, conv_layer in enumerate(self.conv_layers):
-                    with tf.variable_scope("conv_layer_{}".format(conv_i)):
-                        outputs = slim.conv2d(outputs, num_outputs=conv_layer, scope="conv2d_{}x{}".format(self.kernel[0], self.kernel[1]))
+        with tf.name_scope(name), tf.variable_scope("generator", reuse=self.reuse):
 
-                        logging.debug("Output of conv layer {} is {}".format(conv_i, conv_layer))
+            with tf.variable_scope("first_conv_layer"):
+                first_conv_layer = slim.conv2d(output_tensor,
+                                                stride=1,
+                                                padding="SAME",
+                                                num_outputs=self.resnet_output_num,
+                                                kernel_size=self.external_kernel_size,
+                                                activation_fn=self.activation_fn,
+                                                normalizer_fn=None,
+                                                scope="conv2d_{}x{}".format(self.external_kernel_size, self.external_kernel_size)
+                                                )
+                logging.debug("Output of first convolutional layer: {}".format(first_conv_layer))
 
-                for upsampling_i, upsampling in enumerate(self.upsampling_layers):
-                    with tf.variable_scope("upsampling_{}".format(upsampling_i)):
-                        outputs = slim.conv2d_transpose(outputs, num_outputs=upsampling, stride=2, scope="deconv2d_{}x{}".format(self.kernel[0], self.kernel[1]))
+            after_resblocks = self._resblocks(first_conv_layer, is_training=is_training)
+            logging.debug("Output of resblocks: {}".format(after_resblocks))
+            output_tensor = after_resblocks + first_conv_layer
+            logging.debug("Output of sum: {}".format(output_tensor))
 
-                        logging.debug("Output of upsampling layer {} is {}".format(upsampling_i, upsampling))
+            output_tensor = self._upsampling_layers(output_tensor)
+            logging.debug("Output after upsampling: {}".format(output_tensor))
 
-            with tf.variable_scope("generator_output"):
-                outputs = slim.conv2d(outputs,
+            with tf.variable_scope("projection_to_image"):
+                outputs = slim.conv2d(output_tensor,
                                       num_outputs=3,
-                                      kernel_size=self.kernel,
+                                      kernel_size=self.external_kernel_size,
                                       stride=1,
                                       padding='SAME',
                                       activation_fn=tf.nn.tanh,
-                                      normalizer_fn=None,
-                                      normalizer_params=None,
-                                      scope="tanh_conv2d_{}x{}".format(self.kernel[0], self.kernel[1]))
+                                      normalizer_fn=None)
 
-                logging.debug("Output of upsampling generator is {}".format(outputs))
-                tf.summary.image("upsampled", outputs, 5)
-
+            tf.summary.image("enhanced", outputs, max_outputs=5)
         self.reuse = True
         self.variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='generator')
 
@@ -76,7 +138,7 @@ class SimpleGenerator(object):
 
 class Discriminator(object):
 
-    def __init__(self, layer_depths, kernel, activation_fn=tf.nn.relu):
+    def __init__(self, layer_depths, kernel, activation_fn=tf.nn.leaky_relu):
         # TODO: Extract properties into configuraion
         self.layer_depths = layer_depths
         self.reuse = False
@@ -102,30 +164,37 @@ class Discriminator(object):
             tf.variable_scope('discriminator', reuse=self.reuse),\
             slim.arg_scope([slim.conv2d],
                            kernel_size=self.kernel,
-                           stride=2,
                            activation_fn=self.activation_fn,
-                           normalizer_fn=self.normalizer_fn,
-                           normalizer_params=norm_params,
+                           normalizer_fn=None,
                            padding='SAME'):
 
-            for conv_layer_i, conv_layer in enumerate(self.layer_depths):
+            with tf.variable_scope("first_conv"):
+                outputs = slim.conv2d(outputs,
+                                      num_outputs=self.layer_depths[0],
+                                      stride=1)
+                outputs = slim.conv2d(outputs,
+                                      num_outputs=self.layer_depths[0],
+                                      stride=2,
+                                      padding='SAME',
+                                      normalizer_fn=self.normalizer_fn,
+                                      normalizer_params=norm_params)
+
+            for conv_layer_i, conv_layer in enumerate(self.layer_depths[1:]):
 
                 with tf.variable_scope("conv_{}".format(conv_layer_i)):
-
-                    # Don't apply batch norm on first layer (from paper)
-                    if conv_layer_i == 0:
-                        outputs = slim.conv2d(inputs=outputs,
-                                              num_outputs=conv_layer,
-                                              normalizer_fn=None,
-                                              normalizer_params=None,
-                                              stride=1)
-                    else:
-                        outputs = slim.conv2d(inputs=outputs, num_outputs=conv_layer)
+                    outputs = slim.conv2d(inputs=outputs, num_outputs=conv_layer, normalizer_fn=self.normalizer_fn, normalizer_params=norm_params, stride=1)
+                    outputs = slim.conv2d(inputs=outputs, num_outputs=conv_layer, normalizer_fn=self.normalizer_fn, normalizer_params=norm_params, stride=2)
                     logging.debug("Layer {}: {}".format(conv_layer_i, outputs))
 
-            with tf.variable_scope("logits"):
-                outputs = slim.flatten(outputs, scope="flatten_output")
-                logits = slim.fully_connected(inputs=outputs,
+            features = slim.flatten(outputs)
+
+            with tf.variable_scope("full_connected"):
+                dense = slim.fully_connected(inputs=features,
+                                             num_outputs=1024,
+                                             normalizer_fn=None,
+                                             activation_fn=self.activation_fn)
+
+                logits = slim.fully_connected(inputs=dense,
                                                num_outputs=1,
                                                activation_fn=None,
                                                normalizer_fn=None,
@@ -135,20 +204,28 @@ class Discriminator(object):
 
             self.reuse = True
             self.variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='discriminator')
-            return logits
+            return logits, features
 
 
 class SuperResolution(object):
 
-    def __init__(self, generator, discriminator):
+    def __init__(self, generator, discriminator, feature_extractor=None):
         self.generator = generator
         self.discriminator = discriminator
+        self.feature_extractor = None
 
     def loss(self, lowres_input, highres_input, gama):
         enhanced = self.enhance(lowres_input, name='enhance', is_training=True)
 
-        logits_lowres = self.discriminator.discriminate(enhanced, 'lowres_discriminator')
-        logits_highres = self.discriminator.discriminate(highres_input, 'highres_discriminator')
+        logits_lowres, lowres_features = self.discriminator.discriminate(enhanced, 'lowres_discriminator')
+        logits_highres, highres_features = self.discriminator.discriminate(highres_input, 'highres_discriminator')
+
+        if self.feature_extractor:
+            lowres_features = self.feature_extractor.extract(enhanced)
+            highres_features = self.feature_extractor.extract(highres_input)
+
+        lowres_features = 0.006 * lowres_features
+        highres_features = 0.006 * highres_features
 
         highres_discriminator_loss = tf.reduce_mean(
             tf.nn.sigmoid_cross_entropy_with_logits(logits=logits_highres,
@@ -162,6 +239,9 @@ class SuperResolution(object):
         )
         variable_summaries(lowres_discriminator_loss, 'discriminator_generated/summary', 'loss')
 
+        content_loss = tf.reduce_mean(tf.square(highres_features - lowres_features), name="content_loss")
+        variable_summaries(content_loss, "content_loss", "loss")
+
         generator_loss = tf.reduce_mean(
             tf.nn.sigmoid_cross_entropy_with_logits(logits=logits_lowres,
                                                     labels=tf.ones_like(logits_lowres, dtype=tf.float32))
@@ -171,7 +251,7 @@ class SuperResolution(object):
         reconstruction_loss = tf.reduce_mean(tf.square(highres_input - enhanced))
         variable_summaries(reconstruction_loss, 'reconstruction/summary', 'loss')
 
-        total_generator_loss = gama * reconstruction_loss + generator_loss
+        total_generator_loss = content_loss + gama*generator_loss
         variable_summaries(total_generator_loss, 'generator_total/summary', 'loss')
 
         total_discriminator_loss = highres_discriminator_loss + lowres_discriminator_loss
@@ -200,21 +280,16 @@ if __name__ == '__main__':
     import sys
     logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format='[%(asctime)s] [%(levelname)s]: %(message)s')
 
-    lowres_placeholder = tf.placeholder(tf.float32, [None, 64, 64, 3])
+    lowres_placeholder = tf.placeholder(tf.float32, [None, 32, 32, 3])
     highres_placeholder = tf.placeholder(tf.float32, [None, 128, 128, 3])
 
-    generator = SimpleGenerator(conv_layers=[64, 128],
-                                upsampling_layers=[64],
-                                kernel=[5, 5],
-                                activation_fn=tf.nn.relu)
-    discriminator = Discriminator(layer_depths=[64, 96, 128, 160, 256],
-                                  kernel=[5, 5],
-                                  activation_fn=tf.nn.relu)
+    generator = Generator(parametric_relu_fn())
+    discriminator = Discriminator([64, 128, 256, 512], 3, tf.nn.leaky_relu)
+    superresolution = SuperResolution(generator, discriminator)
 
-    super_resolution = SuperResolution(generator, discriminator)
-    d_loss, g_loss = super_resolution.loss(lowres_placeholder, highres_placeholder)
-    train_op = super_resolution.train_op(d_loss, g_loss, 0.0002, 0.6)
+    discriminator_loss, generator_loss = superresolution.loss(lowres_placeholder, highres_placeholder, 1e-3)
 
-    logging.debug("VARIABLES")
-    for var in tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES):
-        logging.debug(var)
+    logging.debug(discriminator_loss)
+    logging.debug(generator_loss)
+
+    train_op = superresolution.train_op(discriminator_loss, generator_loss, 0.0002, 0.7)
